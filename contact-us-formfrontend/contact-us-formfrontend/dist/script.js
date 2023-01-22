fafe449bc8333e2fda9143e44b3a6b6693f77c04
DRAGON SAMA <51018664+DragSama94@users.noreply.github.com>
2023-01-22 15:44:35 +0530

    Add files via upload

---
+++ contact-us-formfrontend/contact-us-formfrontend/dist/script.js
@@ -0,0 +1,7 @@
+// You should totally check out my Software startup! idea42.co 
+// facebook.com/idea42co
+// twitter.com/idea42co
+
+function load() {
+    window.location.href = "../../../shopping-website/shopping-website/dist/index.html";
+}
