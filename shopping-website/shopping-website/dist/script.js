3eafdf6034f15643c24202afc71edf2f6b69459d
DRAGON SAMA <51018664+DragSama94@users.noreply.github.com>
2023-01-22 15:43:34 +0530

    Add files via upload

---
+++ shopping-website/shopping-website/dist/script.js
@@ -0,0 +1,7 @@
+function load() {
+    window.location.href = "../../../contact-us-formfrontend/contact-us-formfrontend/dist/index.html";
+}
+
+function loadSignout() {
+    window.location.href = "../../../index.html";
+}
